54b58ef18d640683a8d3f3df75483d54dbc4f227
Wolf-Quiteque <marcioqui3@gmail.com>
2023-10-25 08:29:41 +0100

    first commit

---
+++ pages/api/aef/allactividades.js
@@ -0,0 +1,12 @@
+import clientPromise from "../../../lib/mongodb";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  const cliente = await clientPromise;
+  const db = cliente.db("aef");
+  const resul = await db.collection("actividades").find({}).toArray();
+  res.json(resul);
+}
